980cae9bb94cc0176aafc0dd8c54a7a0fb966692
João Paulo <68424312+Dwho-O@users.noreply.github.com>
2021-11-23 10:38:17 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name="robotframework-notifications_v1",
+    name="rbt-notifications_v1",
     version="1.2.0",
     description="Envie notigicações com o Robot Framework para o Slack.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/tlolkema/RobotNotifications_V1",
+    url="https://github.com/Dwho-O/RobotNotifications_V1",
     author="Tim Lolkema",
     author_email="tim@detesters.nl",
     license="MIT",
